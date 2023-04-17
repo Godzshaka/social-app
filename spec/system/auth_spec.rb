ec366cfb71d6463b234e312792a6a14caf3a72f6
godzshaka <yagokades@hotmail.com>
2023-04-16 21:50:05 -0300

    created first test

--- spec/system/auth_spec.rb
+++ spec/system/auth_spec.rb
@@ -3,9 +3,13 @@ require 'rails_helper'
 RSpec.describe "Auth", type: :system do
   before do
     driven_by(:rack_test)
-    visit root_path
-    click_on
   end
+  it 'visits and try to sign in' do
+    visit root_path
+    click_on "Create a new account"
+    fill_in "Insert your email address", "john_doe@example.com"
 
-  pending "add some scenarios (or delete) #{__FILE__}"
+
+    pending "add some scenarios (or delete) #{__FILE__}"
+  end
 end
